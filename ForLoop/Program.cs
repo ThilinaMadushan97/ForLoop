01b315dee4fc9e844ffe78c086f7c557b136bd1a
dell <you@example.com>
2024-04-11 00:31:51 +0530

    Add project files.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿for(int i=0; i<100; i++)
+{
+   
+
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+
+        Console.WriteLine($"{i} = Freez & Burzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} = Freez");
+    }
+    else if(i % 5 == 0)
+    {
+        Console.WriteLine($"{i} = Burzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
